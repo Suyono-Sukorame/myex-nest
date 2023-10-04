30ebc3d49cfe9cad877a7517f125c2d0ab9c5aec
Suyono-Sukorame <suyonosukorame@ymail.com>
2023-10-04 21:45:58 +0800

    user

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,28 +1,27 @@
-import { Module } from '@nestjs/common';
-// import { AppController } from './app.controller';
-// import { AppService } from './app.service';
-// import { UsersModule } from './users/users.module';
-// import { ItemsModule } from './items/items.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from 'src/app.controller';
-import { AppService } from 'src/app.service';
-import { UsersModule } from './users.module';
-import { ItemsModule } from 'src/items/items.module';
-import { User } from './users.service';
+import {
+    AfterInsert,
+    Entity,
+    Column,
+    PrimaryGeneratedColumn 
+} from "typeorm";
 
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn()
+    id: number;
 
-@Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'db.sqlite',
-      entities: [],
-      synchronize: true,
-    }),
-    UsersModule,
-    ItemsModule
-  ],
-  controllers: [AppController],
-  providers: [AppService],
-})
-export class AppModule {}
+    @Column()
+    name: string;
+
+    @Column()
+    email: string;
+
+    @Column()
+    password:string;
+
+    @AfterInsert()
+
+    logInsert() {
+        console.log('Inserted User with id: ' + this.id);
+    }
+}
